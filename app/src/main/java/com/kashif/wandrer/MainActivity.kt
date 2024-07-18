77f5579d354eaac4716d0eb3cef009524ddda3b9
Kashif Mehmood <kashismails@gmail.com>
2024-07-18 22:46:14 +0500

    added more configurations

--- app/src/main/java/com/kashif/wandrer/MainActivity.kt
+++ app/src/main/java/com/kashif/wandrer/MainActivity.kt
@@ -8,8 +8,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.kashif.wandrer.ui.theme.WandrerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +29,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MapScreen()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WandrerTheme {
-        Greeting("Android")
+    @Composable
+    private fun MapScreen() {
+        //33.68744 73.05102
+        // Create lat long for icci
+        //create camera position
+        //show google map
+        //add marker
     }
+
 }
